1cb7bf2ae03a4bc0b71d8e6d97455c1ecb36afc1
Jasper Mullican <Japperdo14@outlook.com>
2021-04-25 16:24:25 -0400

    Jaspers Commit

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ print("My name is Cooper and I am only slightly disappointed in my students toda
 print("My name is David  depression is my game if you think qabout we are all the same\n")
 print("My name is David and i am about to really disappoint the teacher\n")
 print("My name is Sam, Sam The Scam of course\n")
-print("My name is Jasper, love is like a fart, if you have to force it, its most likely crap"\")
+print("My name is Jasper, love is like a fart, if you have to force it, its most likely crap\n")
