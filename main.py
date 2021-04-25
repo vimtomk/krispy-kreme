89158e35e36a4283f4f9da742226f3f967fad5de
David Hudry <davidhudry1@gmail.com>
2021-04-25 16:03:39 -0400

    David's Commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 # main.py - by Cooper Yancey and other contributors
 print("My name is Cooper and I am only slightly disappointed in my students today...\n")
-print("My name is David and i am about to really disappoint the teacher")
+print("My name is David
+        depression is my game
+        if you think qabout we are all the same")
