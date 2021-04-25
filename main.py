82178b3b5eddecec7956c97ac53e524ea7b76535
David Hudry <davidhudry1@gmail.com>
2021-04-25 16:07:04 -0400

    David's Commit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # main.py - by Cooper Yancey and other contributors
 print("My name is Cooper and I am only slightly disappointed in my students today...\n")
 <<<<<<< HEAD
-print("My name is David
-        depression is my game
-        if you think qabout we are all the same\n")
+print("My name is David  depression is my game if you think qabout we are all the same\n")
 =======
 print("My name is David and i am about to really disappoint the teacher\n")
 print("My name is Sam, Sam The Scam of course\n")
