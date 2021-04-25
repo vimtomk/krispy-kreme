6e42596592f383cad91e21b97dd829f11d1af9a7
Jasper Mullican <Japperdo14@outlook.com>
2021-04-25 16:14:53 -0400

    fixed whoever put that awful line

--- main.py
+++ main.py
@@ -3,4 +3,3 @@ print("My name is Cooper and I am only slightly disappointed in my students toda
 print("My name is David  depression is my game if you think qabout we are all the same\n")
 print("My name is David and i am about to really disappoint the teacher\n")
 print("My name is Sam, Sam The Scam of course\n")
->>>>>>> fa2e6f6fa5a913c31f403d21ed2e1c7bce2f2fa9
