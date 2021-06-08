12458586fe048842a2e2ae11a01ec78827560d3f
Cooper Yancey <cooper.yancey@outlook.com>
2021-06-08 18:56:25 -0500

    Added some comments and a file.

--- fib.py
+++ fib.py
@@ -1,7 +1,15 @@
 # Fibonnaci example:
 # sum of two elements defines next element
+
+# Setting up variables needed.
 a, b = 0, 1
 
+# Run inside while loop until the output is greater than or equal to 10.
 while a < 10:
+#while True:
+
+    # Print the current value of A
     print(a)
+
+    # A gets B (current value), B gets A+B (next value)
     a, b = b, a+b
