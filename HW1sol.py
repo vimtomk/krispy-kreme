5c0ba5c7e1070227d4173735faad9e241cb3b24c
Cooper Yancey <cooper.yancey@outlook.com>
2021-06-13 18:29:00 -0500

    Added homework 1 solution.

---
+++ HW1sol.py
@@ -0,0 +1,13 @@
+# Solution to HW 1
+# Calculates a value "x" to the power of 0->10
+
+# Setting up variables. A is the power, and B is the numerator.
+a, b = 0, 2
+
+# While loop that runs through 0->10
+#while a < 11:
+while b == 2:
+    # Printing output
+    print(b**a)
+    # Incrementing power
+    a = a + 1
